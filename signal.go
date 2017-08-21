31b6d31afaa45979b1862470cef997c39aea1ceb
libinbin <libinbin@17paipai.cn>
2017-08-21 20:09:08 +0800

    add signal

---
+++ signal.go
@@ -0,0 +1,21 @@
+package lbbutil
+
+import (
+	"os"
+	"os/signal"
+)
+
+func RegistSignal(handle func(), sig ...os.Signal) {
+	c := make(chan os.Signal, len(sig))
+	signal.Notify(c, sig...)
+	go func() {
+		<-c
+		handle()
+	}()
+}
+
+func MakeSignal(sig ...os.Signal) chan os.Signal {
+	c := make(chan os.Signal, len(sig))
+	signal.Notify(c, sig...)
+	return c
+}
