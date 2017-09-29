cc9740071c0dc9f6ed16cb33bf84875c42c14b1e
libinbin <libinbin@17paipai.cn>
2017-09-29 18:09:57 +0800

    add

--- conv.go
+++ conv.go
@@ -16,3 +16,15 @@ func ToUint64(data string) uint64 {
 	}
 	return a
 }
+
+func ToInt64(data string) int64 {
+	a, err := strconv.ParseInt(data, 10, 64)
+	if err != nil {
+		return -1
+	}
+	return a
+}
+func Int64ToString(value int64) string {
+	a := strconv.FormatInt(value, 64)
+	return a
+}
